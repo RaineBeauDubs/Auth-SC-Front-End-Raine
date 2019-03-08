01b43ede1c4a7f1fd50799bdb07b66ba8be82185
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-08 12:59:02 -0500

    Completed log-out functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,12 @@ import Login from './login/Login';
 import Jokes from './jokes/Jokes';
 
 class App extends Component {
+
+  logout = () => {
+    localStorage.removeItem('jwt');
+    this.props.history.push('/login');
+  };
+
   render() {
     return (
       <div className="App">
@@ -12,6 +18,7 @@ class App extends Component {
           <nav>
             <NavLink to='/login'>Log-In</NavLink>
             <NavLink to='/jokes'>Jokes</NavLink>
+            <button onClick={this.logout}>Log-Out</button>
           </nav>
         </header>
         <h2>Welcome to Jokes!</h2>
